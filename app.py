346dc84b314e2cbe4467156fd0633681d9fa7cee
Kagan Tek <90055268+kagantek@users.noreply.github.com>
2025-06-05 17:45:47 +0300

    Added method for ResNet50 Mammography Prediction

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.preprocessing.image import load_img, img_to_array, array_to_img
+from tensorflow.keras.applications.densenet import DenseNet121, preprocess_input
+from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
+from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
+from tensorflow.keras.models import load_model
+import os
 
 app = Flask(__name__)
+
 
 @app.route('/')
 @app.route('/index')
@@ -51,9 +61,37 @@ def vgg_histopathology_notebook():
 def densenet_histopathology_notebook():
     return render_template('densenet_histopathology_notebook.html')
 
-@app.route('/resnet_mammography')
+@app.route('/resnet_mammography', methods=['GET', 'POST'])
 def resnet_mammography():
+    if request.method == 'POST':
+        imagefile = request.files['imagefile']
+
+        images_dir = "./static/images"
+        if not os.path.exists(images_dir):
+            os.makedirs(images_dir)
+
+        image_path = os.path.join(images_dir, imagefile.filename)
+        imagefile.save(image_path)
+
+        image = load_img(image_path, target_size=(224, 224))
+        image = img_to_array(image)
+        image = np.expand_dims(image, axis=0)
+
+        print("Loading ResNet50 Mammography Model...")
+        modelResnet = load_model('./model/mammography/mammography_resnet.h5', compile=False, custom_objects={'preprocess_input': preprocess_input})
+        print("Model loaded successfully!")
+
+        yhat = modelResnet.predict(image, verbose=0)
+        prob = float(yhat[0][0])
+        TAU = 0.49
+        label = "Tumor Detected" if prob >= TAU else "No Tumor Detected"
+        conf = prob if prob >= TAU else 1 - prob
+        prediction = f"{label} ({conf*100:.2f}%) (Threshold: {TAU})"
+
+        return render_template('resnet_mammography.html', prediction=prediction)
+
     return render_template('resnet_mammography.html')
+
 
 @app.route('/vgg_mammography')
 def vgg_mammography():
